0f5924790728a4c12a6c7b887232394db9e1164f
Alcygo <alimyuparham@gmail.com>
2025-06-27 18:17:08 +0700

    Update Nav-slider.js

--- Nav-slider.js
+++ Nav-slider.js
@@ -15,6 +15,7 @@ $("#LinkList96").each(function () {
         if ($(this).html().replace(/\s|&nbsp;/g, "").length == 0) $(this).remove();
     });
 });
+
 // TAMBAHAN MENU INTERAKTIF
 $(document).ready(function () {
     $("#menu").show();
@@ -25,9 +26,11 @@ $(document).ready(function () {
             function () { $(this).children("ul").hide(); }
         );
     });
+
     $('#search-icon').on('click', function () {
         $('#nav-search').slideToggle(250);
     });
+
     // TOMBOL SCROLL KE ATAS
     $(window).scroll(function () {
         if ($(this).scrollTop() > 1000) {
@@ -41,6 +44,7 @@ $(document).ready(function () {
         $('body,html').animate({ scrollTop: 0 }, 800);
         return false;
     });
+
     // EFEK GAMBAR SCROLL
     $('body').addClass('img-anime');
     $(window).on('load resize scroll', function () {
@@ -62,4 +66,105 @@ $('.slider-wrapper .HTML .widget-content').each(function () {
         b1 = "recent",
         b2 = "label",
         box = $(this).find("div").attr("class");
+
+    function buildSlider(data) {
+        var url = "";
+        var featcode = '<div id="featured-slider"><ul class="slides">';
+        for (var i = 0; i < data.feed.entry.length; i++) {
+            for (var j = 0; j < data.feed.entry[i].link.length; j++) {
+                if (data.feed.entry[i].link[j].rel == "alternate") {
+                    url = data.feed.entry[i].link[j].href;
+                    break;
+                }
+            }
+            var title = data.feed.entry[i].title.$t;
+            var author_name = data.feed.entry[i].author[0].name.$t;
+            var get_date = data.feed.entry[i].published.$t,
+                year = get_date.substring(0, 4),
+                month = get_date.substring(5, 7),
+                day = get_date.substring(8, 10),
+                date = MONTH_FORMAT[parseInt(month, 10)] + ' ' + day + ', ' + year;
+            var tag = data.feed.entry[i].category[0].term;
+            var content = data.feed.entry[i].content.$t;
+            var $content = $('<div>').html(content);
+            var image = NO_IMAGE;
+            if (content.indexOf("http://www.youtube.com/embed/") > -1 || content.indexOf("https://www.youtube.com/embed/") > -1) {
+                image = data.feed.entry[i].media$thumbnail.url;
+            } else if (content.indexOf("<img") > -1) {
+                image = $content.find('img:first').attr('src');
+            }
+
+            featcode += '<li><a class="slider-img" href="' + url + '"><div class="feets" style="background-image:url(' + image + ');"></div></a><div class="slide-cap"><h1 class="post-title"><a href="' + url + '">' + title + '</a></h1><span class="feat-divider"></span><span class="post-date">' + date + '</span></div><div class="slide-cap-bg"></div><a href="' + url + '" class="slide-overlay"></a></li>';
+        }
+        featcode += '</ul><div id="slider-nav"/></div>';
+        return featcode;
+    }
+
+    if (box.match(b1)) {
+        $.ajax({
+            url: "/feeds/posts/default?alt=json-in-script&max-results=" + num,
+            type: 'get',
+            dataType: "jsonp",
+            success: function (data) {
+                var featcode = buildSlider(data);
+                $('.slider-wrapper .HTML .widget-content').each(function () {
+                    if ($(this).find("div").attr("class").match("recent")) {
+                        $(this).html(featcode);
+                        $(this).removeClass('widget-content').addClass('slider-content');
+                        $('#featured-slider').flexslider({
+                            controlsContainer: '#slider-nav',
+                            controlNav: false,
+                            pauseOnAction: false,
+                            pauseOnHover: true,
+                            animation: 'fade',
+                            animationSpeed: 1200,
+                            slideshowSpeed: 7000,
+                            prevText: '',
+                            nextText: ''
+                        });
+                        $(this).find('.rcp-thumb').each(function () {
+                            $(this).attr('style', function (i, src) {
+                                return src.replace('/default.jpg', '/hqdefault.jpg')
+                            }).attr('style', function (i, src) {
+                                return src.replace('s72-c', 's1600')
+                            });
+                        });
+                    }
+                });
+            }
+        });
+    } else if (box.match(b2)) {
+        $.ajax({
+            url: "/feeds/posts/default/-/" + cat + "?alt=json-in-script&max-results=" + num,
+            type: 'get',
+            dataType: "jsonp",
+            success: function (data) {
+                var featcode = buildSlider(data);
+                $('.slider-wrapper .HTML .widget-content').each(function () {
+                    if ($(this).find("div").attr("class").match("label")) {
+                        $(this).html(featcode);
+                        $(this).removeClass('widget-content').addClass('slider-content');
+                        $('#featured-slider').flexslider({
+                            controlsContainer: '#slider-nav',
+                            controlNav: false,
+                            pauseOnAction: false,
+                            pauseOnHover: true,
+                            animation: 'fade',
+                            animationSpeed: 1200,
+                            slideshowSpeed: 7000,
+                            prevText: '',
+                            nextText: ''
+                        });
+                        $(this).find('.rcp-thumb').each(function () {
+                            $(this).attr('style', function (i, src) {
+                                return src.replace('/default.jpg', '/hqdefault.jpg')
+                            }).attr('style', function (i, src) {
+                                return src.replace('s72-c', 's1600')
+                            });
+                        });
+                    }
+                });
+            }
+        });
+    }
 });
